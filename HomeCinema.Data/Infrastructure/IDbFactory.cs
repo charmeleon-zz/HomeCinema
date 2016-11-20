fa0451a0eb47c516fd29c32bfa0bc8bb2cdf8449
Erick Rivas <erivas@birkman.com>
2016-11-20 13:34:37 -0600

    Save progress on data repositories

---
+++ HomeCinema.Data/Infrastructure/IDbFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using HomeCinema.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCinema.Data.Infrastructure
+{
+    public interface IDbFactory : IDisposable
+    {
+        HomeCinemaContext Init();
+    }
+}
