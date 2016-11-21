8968fb9ac9583b5a2626d311aaed6c2a21f8ae60
Erick Rivas <erivas@birkman.com>
2016-11-20 21:36:55 -0600

    Begin front-end solution for HomeCinema

--- HomeCinema.Services/Abstract/IMembershipService.cs
+++ HomeCinema.Services/Abstract/IMembershipService.cs
@@ -1,10 +1,5 @@
 ﻿using HomeCinema.Entities;
-using HomeCinema.Services.Utilities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeCinema.Services
 {
---
+++ HomeCinema.Web/App_Start/AutofacWebapiConfig.cs
@@ -0,0 +1,49 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using HomeCinema.Data;
+using HomeCinema.Data.Infrastructure;
+using HomeCinema.Data.Repositories;
+using HomeCinema.Services;
+using System.Data.Entity;
+using System.Reflection;
+using System.Web.Http;
+
+namespace HomeCinema.Web.App_Start
+{
+    public class AutofacWebApiConfig
+    {
+        public static IContainer Container;
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config, RegisterServices(new ContainerBuilder()));
+        }
+        private static IContainer RegisterServices(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            containerBuilder.RegisterType<HomeCinemaContext>()
+                .As<DbContext>()
+                .InstancePerRequest();
+            containerBuilder.RegisterType<DbFactory>()
+                .As<IDbFactory>()
+                .InstancePerRequest();
+            containerBuilder.RegisterType<UnitOfWork>()
+                .As<IUnitOfWork>()
+                .InstancePerRequest();
+            containerBuilder.RegisterGeneric(typeof(EntityBaseRepository<>))
+                .As(typeof(IEntityBaseRepository<>))
+                .InstancePerRequest();
+            // Services
+            containerBuilder.RegisterType<EncryptionService>()
+                .As<IEncyptionService>()
+                .InstancePerRequest();
+            Container = containerBuilder.Build();
+
+            return Container;
+        }
+
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
