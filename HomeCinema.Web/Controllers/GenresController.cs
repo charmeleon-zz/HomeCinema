01aebc78db3408c61f1b63b2e2c098e3a360bc0b
Erick Rivas <erivas@birkman.com>
2016-11-25 16:49:13 -0600

    Genre, movie controllers

---
+++ HomeCinema.Web/Controllers/GenresController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using HomeCinema.Data.Infrastructure;
+using HomeCinema.Data.Repositories;
+using HomeCinema.Entities;
+using HomeCinema.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace HomeCinema.Web.Controllers
+{
+    [Authorize(Roles="Admin")]
+    [RoutePrefix("api/genres")]
+    public class GenresController : ApiControllerBase
+    {
+        private readonly IEntityBaseRepository<Genre> _genreRepository;
+        public GenresController(IEntityBaseRepository<Genre> genreRepo,
+            IEntityBaseRepository<Error> errorRepo, IUnitOfWork _unitOfWork)
+            : base(errorRepo, _unitOfWork)
+        {
+            _genreRepository = genreRepo;
+        }
+
+        [AllowAnonymous]
+        public HttpResponseMessage Get(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                var genres = _genreRepository.GetAll().ToList();
+                var genresVm = Mapper.Map<IEnumerable<Genre>, IEnumerable<GenreViewModel>>(genres);
+
+                response = request.CreateResponse<IEnumerable<GenreViewModel>>(HttpStatusCode.OK, genresVm);
+
+                return response;
+            });
+        }
+    }
+}
---
+++ HomeCinema.Web/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using HomeCinema.Data.Infrastructure;
+using HomeCinema.Data.Repositories;
+using HomeCinema.Entities;
+using HomeCinema.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HomeCinema.Web.Controllers
+{
+    public class MoviesController : ApiControllerBase
+    {
+        private readonly IEntityBaseRepository<Movie> movieRepository;
+
+        public MoviesController(IEntityBaseRepository<Movie> movieRepo, 
+            IEntityBaseRepository<Error> errorRepo, IUnitOfWork unitOfWork)
+            : base(errorRepo, unitOfWork)
+        {
+            movieRepository = movieRepo;
+        }
+
+        [AllowAnonymous]
+        [Route("latest")]
+        public HttpResponseMessage Get(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+
+                var movies = movieRepository.GetAll().OrderByDescending(m => m.ReleaseDate).Take(6).ToList();
+                var moviesVm = Mapper.Map<IEnumerable<Movie>, IEnumerable<MovieViewModel>>(movies);
+
+                response = request.CreateResponse<IEnumerable<MovieViewModel>>(HttpStatusCode.OK, moviesVm);
+
+                return response;
+            });
+        }
+    }
+}
