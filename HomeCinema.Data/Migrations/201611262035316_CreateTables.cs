f165b1200565a92b505b83d5265fc065c0614f3b
Erick Rivas <erivas@birkman.com>
2016-11-26 14:36:51 -0600

    Migrations do-over

--- HomeCinema.Data/Migrations/201611262035316_CreateTables.cs
+++ HomeCinema.Data/Migrations/201611262035316_CreateTables.cs
@@ -3,7 +3,7 @@ namespace HomeCinema.Data.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class InitialMigration : DbMigration
+    public partial class CreateTables : DbMigration
     {
         public override void Up()
         {
